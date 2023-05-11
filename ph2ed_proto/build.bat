73e0acff3c48f700ce3d70b7a0c54d6b561d6f50
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2023-05-10 22:03:10 -0400

    Editor: Integration solution file

--- ph2ed_proto/build.bat
+++ ph2ed_proto/build.bat
@@ -7,9 +7,9 @@ if !errorlevel! neq 0 goto end
 REM Debug modes:
 if "%1" neq "Release" (
 
-cl       -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505                                 -EHa- main.cpp -c -Fomain.obj -RTCu
+cl       -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505                                 -EHa- main.cpp -c -Fomain.obj -RTCu -GS-
 if !errorlevel! neq 0 goto end
-cl       -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505 -wd4334 -wd4244 -wd4267 -wd4706 -EHa- libs.cpp -c -Folibs.obj -RTCu
+cl       -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505 -wd4334 -wd4244 -wd4267 -wd4706 -EHa- libs.cpp -c -Folibs.obj -RTCu -GS-
 if !errorlevel! neq 0 goto end
 REM clang-cl -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505                                 -Wno-missing-braces -Wno-unused-variable -Wno-unused-but-set-variable -Wno-sign-compare -Wno-tautological-constant-out-of-range-compare -Wno-missing-field-initializers -Wno-unused-function -EHa- -fsanitize=address -fsanitize=undefined main.cpp -c -Fomain.obj -mavx2 -maes
 if !errorlevel! neq 0 goto end
@@ -26,7 +26,7 @@ if !errorlevel! neq 0 goto end
 ) else (
 REM Release mode:
 
-cl                    -DNDEBUG -O2 -Z7 -nologo -EHa- main.cpp -Feph2ed_proto.exe -link -noimplib -noexp -incremental:no
+cl                    -DNDEBUG -O2 -Z7 -nologo -EHa- main.cpp -Feph2ed_proto.exe -GS- -link -noimplib -noexp -incremental:no
 if !errorlevel! neq 0 goto end
 REM clang-cl -fuse-ld=lld -DNDEBUG -O2 -Z7 -nologo -EHa- main.cpp -Feph2ed_proto.exe -mavx2 -maes
 if !errorlevel! neq 0 goto end
