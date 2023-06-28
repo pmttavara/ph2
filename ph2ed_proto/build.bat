8863a3833c481be0534075cd439d6a96172a1586
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2023-06-28 01:23:48 -0400

    Editor: Switch to a Ninja build script

--- ph2ed_proto/build.bat
+++ ph2ed_proto/build.bat
@@ -1,39 +1,6 @@
 @echo off
-setlocal enabledelayedexpansion
 
-sokol-shdc -i shaders.glsl -o shaders.glsl.h --slang hlsl5 --bytecode
-if !errorlevel! neq 0 goto end
-
-REM Debug modes:
-if "%1" neq "Release" (
-
-cl       -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505                                 -EHa- main.cpp -c -Fomain.obj -RTCu -GS-
-if !errorlevel! neq 0 goto end
-cl       -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505 -wd4334 -wd4244 -wd4267 -wd4706 -EHa- libs.cpp -c -Folibs.obj -RTCu -GS-
-if !errorlevel! neq 0 goto end
-REM clang-cl -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505                                 -Wno-missing-braces -Wno-unused-variable -Wno-unused-but-set-variable -Wno-sign-compare -Wno-tautological-constant-out-of-range-compare -Wno-missing-field-initializers -Wno-unused-function -EHa- -fsanitize=address -fsanitize=undefined main.cpp -c -Fomain.obj -mavx2 -maes
-if !errorlevel! neq 0 goto end
-REM clang-cl -Od -Z7 -nologo -W4 -WX -wd4189 -wd4456 -wd4457 -wd4800 -wd4324 -wd4505 -wd4334 -wd4244 -wd4267 -wd4706 -Wno-missing-braces -Wno-unused-variable -Wno-unused-but-set-variable -Wno-sign-compare -Wno-tautological-constant-out-of-range-compare -Wno-missing-field-initializers -Wno-unused-function -EHa- -fsanitize=address -fsanitize=undefined libs.cpp -c -Folibs.obj -mavx2 -maes
-if !errorlevel! neq 0 goto end
-
-link     -noimplib -noexp -debug -incremental:no -nologo main.obj libs.obj -out:ph2ed_proto.exe
-if !errorlevel! neq 0 goto end
-REM lld-link                  -debug -incremental:no -nologo main.obj libs.obj -out:ph2ed_proto.exe
-if !errorlevel! neq 0 goto end
-REM clang -fuse-ld=lld -g -gfull main.obj libs.obj -o ph2ed_proto.exe -fsanitize=address -fsanitize=undefined
-if !errorlevel! neq 0 goto end
-
-) else (
-REM Release mode:
-
-cl                    -DNDEBUG -O2 -Z7 -nologo -EHa- main.cpp -Feph2ed_proto.exe -GS- -link -noimplib -noexp -incremental:no
-if !errorlevel! neq 0 goto end
-REM clang-cl -fuse-ld=lld -DNDEBUG -O2 -Z7 -nologo -EHa- main.cpp -Feph2ed_proto.exe -mavx2 -maes
-if !errorlevel! neq 0 goto end
-
-)
-
-exit /b 0
-:end
-exit /b 1
-
+set "config=%1"
+if "%1" == "" ( set "config=debug" )
+ninja -f build_%config%.ninja || exit /b %errorlevel%
+copy "build\%1\ph2ed_proto.exe" ph2ed_proto.exe >nul 2>&1 || exit /b %errorlevel%
