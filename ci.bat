2ea71d1307ac51a0e55b62e06975d255c5106ec3
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2023-06-28 01:58:58 -0400

    CI: Fix build

--- ci.bat
+++ ci.bat
@@ -1,3 +1,4 @@
-@echo off
-
-cd ph2ed_proto && build.bat release
+call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat"
+pushd ph2ed_proto
+call "./build.bat" release
+popd
