06f59a76609f374314f03364347e1c445d170def
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2023-06-28 04:37:05 -0400

    CI: Deploy release build

--- ci.bat
+++ ci.bat
@@ -1,3 +1,7 @@
-call "ph2ed_proto/build.bat" debug || exit /b %errorlevel%
+::
+:: This is the Continuous Integration script, don't run this on your PC!
+::
+
+call "ph2ed_proto/build.bat" release || exit /b %errorlevel%
 call powershell Compress-Archive -Force -Path .\ph2ed_proto\ph2ed_proto.exe -DestinationPath ph2ed_proto.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
