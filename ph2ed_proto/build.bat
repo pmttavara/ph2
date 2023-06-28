eb5dd5d6d7eb0685c64b40dfd649806221e18bff
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2023-06-28 19:10:42 -0400

    Editor: Tweak build script

--- ph2ed_proto/build.bat
+++ ph2ed_proto/build.bat
@@ -4,10 +4,9 @@ pushd "%~dp0"
 if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat" call "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat" >nul )
 if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" >nul )
 set "config=%1"
-if "%1" == "" ( set "config=debug" )
+if "%1" == "" ( set "config=release" )
 ninja -f build_%config%.ninja || goto end
-copy "build\%config%\ph2ed_proto.exe" ph2ed_proto.exe || goto end
+copy "build\%config%\ph2ed_proto.exe" ph2ed_proto.exe >nul || goto end
 :end
 popd
-echo Exiting with code %errorlevel%
 exit /b %errorlevel%
