1d3c88657c4ee9f113b964a0f8b1791d095237e6
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2023-06-28 04:14:21 -0400

    CI: Fix build

--- ph2ed_proto/build.bat
+++ ph2ed_proto/build.bat
@@ -1,6 +1,12 @@
 @echo off
 
+pushd "%~dp0"
+if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat" call "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat" >nul )
+if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" >nul )
 set "config=%1"
 if "%1" == "" ( set "config=debug" )
-ninja -f build_%config%.ninja || exit /b %errorlevel%
-copy "build\%1\ph2ed_proto.exe" ph2ed_proto.exe >nul 2>&1 || exit /b %errorlevel%
+ninja -f build_%config%.ninja || goto end
+copy "build/%config%/ph2ed_proto.exe" ph2ed_proto.exe >nul || goto end
+:end
+popd
+exit /b %errorlevel%
