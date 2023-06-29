5ec658e13a7ac56aff4d6aa6d64645ed36d0e404
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2023-06-29 06:44:40 -0400

    Editor: Change build; copy PDB

--- ph2ed_proto/build.bat
+++ ph2ed_proto/build.bat
@@ -5,8 +5,12 @@ if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Enter
 if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" >nul )
 set "config=%1"
 if "%1" == "" ( set "config=release" )
-ninja -f build_%config%.ninja || goto end
+echo git_hash = $> build.ninja
+git rev-parse --short HEAD >> build.ninja
+type build_%config%.ninja >> build.ninja
+ninja || goto end
 copy "build\%config%\ph2ed_proto.exe" ph2ed_proto.exe >nul || goto end
+copy "build\%config%\ph2ed_proto.pdb" ph2ed_proto.pdb >nul || goto end
 :end
 popd
 exit /b %errorlevel%
