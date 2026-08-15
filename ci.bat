5559e2bdeae36991fb6d704243d93e6f7eb619f5
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2026-08-15 22:17:09 +0400

    CI: Fix build

--- ci.bat
+++ ci.bat
@@ -2,6 +2,7 @@
 :: This is the Continuous Integration script, don't run this on your PC!
 ::
 
-call "ph2ed_proto/build.bat" release || exit /b %errorlevel%
-call powershell Compress-Archive -Force -Path .\ph2ed_proto\ph2ed_proto.exe -DestinationPath ph2ed_proto.zip
-if %errorlevel% neq 0 exit /b %errorlevel%
+call "ph2ed_proto/build.bat" release
+if %errorlevel% neq 0 exit /b 1
+call powershell Compress-Archive -Force -Path .\ph2ed_proto\ph2ed_proto.exe,.\ph2ed_proto\ph2ed_proto.pdb -DestinationPath ph2ed_proto.zip
+if %errorlevel% neq 0 exit /b 1
--- ph2ed_proto/build.bat
+++ ph2ed_proto/build.bat
@@ -1,8 +1,16 @@
 @echo off
 
 pushd "%~dp0"
+if not "%LIB%" == "" goto have_msvc
+set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%vswhere%" set "vswhere=%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
+set "vcvars="
+if exist "%vswhere%" for /f "usebackq delims=" %%i in (`"%vswhere%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do set "vcvars=%%i\VC\Auxiliary\Build\vcvars64.bat"
+if exist "%vcvars%" call "%vcvars%" >nul
 if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat" call "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat" >nul )
 if "%LIB%" == "" ( if exist "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" >nul )
+if "%LIB%" == "" ( echo build.bat: no MSVC environment - could not find vcvars64.bat & popd & exit /b 1 )
+:have_msvc
 set "config=%1"
 if "%1" == "" ( set "config=release" )
 echo git_hash = $> build.ninja
